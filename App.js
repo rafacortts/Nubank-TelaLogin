d802836e27c677ecaf463abb435e1fb8cfa92b50
Rafael Cortes Soares <NIT0303999@aluno.firjansenaisesi.com.br>
2024-05-21 16:23:59 -0300

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, View, Image } from 'react-native';
+import TelaLogin from './Components/Header'
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <TelaLogin/>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +13,10 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  image: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
   },
 });
---
+++ Components/Header.js
@@ -0,0 +1,107 @@
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  ImageBackground,
+} from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <ImageBackground
+        source={{
+          uri: "https://nubank.com.br/images-cms/1650479255-woman-holding-card-smiling-2-xs-3x.jpg",
+        }}
+        style={styles.imageBackground}
+      >
+        <TouchableOpacity style={styles.buttonPais}>
+          <Text style={styles.buttonText1}>Pais</Text>
+        </TouchableOpacity>
+
+        <Text style={styles.logo}>
+            NU
+        </Text>
+
+        <View style={styles.loginContainer}>
+          <Text style={styles.title}>
+            Um mundo financeiro sem complexidades{" "}
+          </Text>
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.buttonText}>Começar</Text>
+          </TouchableOpacity>
+        </View>
+      </ImageBackground>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  imageBackground: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  loginContainer: {
+    width: "100%",
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 500,
+  },
+  title: {
+    fontSize: 32,
+    color: "#fff",
+    marginBottom: 20,
+    textAlign: "center",
+    paddingHorizontal: 20,
+    fontFamily: "Roboto",
+  },
+  button: {
+    width: "80%",
+    height: 50,
+    backgroundColor: "#8A05BE",
+    borderRadius: 20,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  buttonText: {
+    color: "#fff",
+    fontSize: 18,
+    fontWeight: "500",
+  },
+  Cadastro: {
+    color: "#FFF",
+    marginTop: 10,
+    textDecorationLine: "underline",
+    fontFamily: "Roboto",
+    fontSize: 16,
+  },
+  buttonPais: {
+    position: "absolute",
+    top: 20, 
+    right: 20, 
+    width: "20%",
+    height: 30,
+    backgroundColor: "",
+    borderRadius: 20,
+    justifyContent: "center",
+    alignItems: "center",
+    border:'1px solid GRAY'
+  },
+  buttonText1: {
+    color: "#FFF",
+    fontSize: 16,
+  },
+  logo:{
+    position: "absolute",
+    top: 20, 
+    left: 20,
+    color :'#FFF',
+    fontSize:30
+  }
+
+});
